cc009756d584a6f860749d068eed93bac2244795
Aleksandr Kovalenko <kovalenlo1234as@yandex.ru>
2026-04-17 04:31:58 +0300

    Добавил валидацию и тесты для User и Film

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,80 @@
-package ru.yandex.practicum.filmorate.controller;
+    package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+    import org.springframework.web.bind.annotation.*;
+    import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+    import ru.yandex.practicum.filmorate.model.Film;
 
-@RestController
-public class FilmController {
-}
+    import java.time.LocalDate;
+    import java.util.Collection;
+    import java.util.HashMap;
+    import java.util.Map;
+
+    @RestController
+    @RequestMapping("/films")
+    public class FilmController {
+
+        private final Map<Long, Film> films = new HashMap<>();
+
+        @GetMapping
+        public Collection<Film> findAll(){
+            return films.values();
+        }
+
+        @PostMapping
+        public Film create(@RequestBody Film film) {
+            if (film.getName() == null || film.getName().isBlank()) {
+                throw new ConditionsNotMetException("Название не может быть пустым");
+            }
+            if (film.getDescription() != null && film.getDescription().length() > 200) {
+                throw new ConditionsNotMetException("Максимальная длина описания 200 символов");
+            }
+
+            LocalDate minReleaseDate = LocalDate.of(1895, 12, 28);
+            if (film.getReleaseDate() == null) {
+                throw new ConditionsNotMetException("Дата релиза должна быть указана");
+            }
+            if (film.getReleaseDate().isBefore(minReleaseDate)) {
+                throw new ConditionsNotMetException("Дата релиза не может быть раньше 28 декабря 1895 года");
+            }
+
+            if (film.getDuration() == null || film.getDuration() <= 0) {
+                throw new ConditionsNotMetException("Продолжительность должна быть положительной");
+            }
+
+            film.setId(getNextId());
+            films.put(film.getId(), film);
+            return film;
+        }
+
+        @PutMapping
+        public Film update(@RequestBody Film newFilm){
+            Film film = films.get(newFilm.getId());
+
+            if (film == null) {
+                throw new ConditionsNotMetException("Фильм не найден");
+            }
+            if (newFilm.getName() != null) {
+                film.setName(newFilm.getName());
+            }
+            if (newFilm.getDescription() != null) {
+                film.setDescription(newFilm.getDescription());
+            }
+            if (newFilm.getReleaseDate() != null) {
+                film.setReleaseDate(newFilm.getReleaseDate());
+            }
+            if (newFilm.getDuration() != null) {
+                film.setDuration(newFilm.getDuration());
+            }
+            return film;
+        }
+
+        private long getNextId() {
+            long currentMaxId = films.keySet()
+                    .stream()
+                    .mapToLong(id -> id)
+                    .max()
+                    .orElse(0);
+            return ++currentMaxId;
+        }
+
+    }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,85 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final Map<Long, User> users = new HashMap<>();
+
+    @GetMapping
+    public Collection<User> findAll() {
+        return users.values();
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            throw new ConditionsNotMetException("Email должен быть указан и содержать символ @");
+        }
+
+        if (user.getLogin() == null || user.getLogin().isBlank()) {
+            throw new ConditionsNotMetException("Login должен быть указан");
+        }
+        if (user.getLogin().contains(" ")) {
+            throw new ConditionsNotMetException("Login не должен содержать пробелы");
+        }
+
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+
+        if (user.getBirthday() == null) {
+            throw new ConditionsNotMetException("Дата рождения должна быть указана");
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ConditionsNotMetException("Дата рождения не может быть в будущем");
+        }
+
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User update(@RequestBody User newUser){
+        if (newUser.getId() == null) {
+            throw new ConditionsNotMetException("Id должен быть указан");
+        }
+
+        User user = users.get(newUser.getId());
+        if (user == null) {
+            throw new ConditionsNotMetException("Пользователь с id " + newUser.getId() + " не найден");
+        }
+
+        if (newUser.getEmail() != null) {
+            user.setEmail(newUser.getEmail());
+        }
+        if (newUser.getName() != null) {
+            user.setName(newUser.getName());
+        }
+        if (newUser.getBirthday() != null) {
+            user.setBirthday(newUser.getBirthday());
+        }
+
+        return user;
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
